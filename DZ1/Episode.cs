4f8c5a255c6ec995b258445222c3c74703e6399d
jurajperic <juraj.peric@gmail.com>
2020-10-25 20:26:08 +0100

    added DZ1

---
+++ DZ1/Episode.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class Episode
+    {
+       int viewers;
+       double maxScore;
+       double totalScore;
+
+        public Episode(int viewers,double totalScore,double maxScore)
+        {
+            this.viewers = viewers;
+            this.totalScore = totalScore;
+            this.maxScore = maxScore;
+        }
+
+        public Episode() { }
+        public void AddView(double score)
+        {
+            viewers++;
+            totalScore += score;
+            if (score > maxScore)
+            {
+                maxScore = score;
+            }
+        }
+
+        public double GetMaxScore()
+        {
+            return maxScore;
+        }
+
+        public int GetViewerCount()
+        {
+            return viewers;
+        }
+
+        public double GetAverageScore()
+        {
+            return totalScore / viewers;
+        }
+
+        
+    }
+}
